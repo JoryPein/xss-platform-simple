f9d7daab3f92908e87524c950d6312a7d908b926
jsbba <729013617@qq.com>
2021-11-19 18:38:07 +0800

    add client

--- readme.md
+++ readme.md
@@ -2,3 +2,14 @@
 
 
 
+## 服务端
+默认监听
+    http 80
+    https 443
+xss.exe -mode server -ip 0.0.0.0 -port 9999 -pass test1234567 -sslcrt c:\\1.crt -sslkey c:\\1.key
+
+## 客服端
+默认web面板监听
+    http 9090
+    
+xss.exe -mode client -ip 1.1.1.1 -port 9999 -pass test1234567
--- xss/main.go
+++ xss/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"flag"
+	"xss/client"
 	"xss/server"
 )
 
+var (
+	mode   string
+	port   string
+	ip     string
+	pass   string
+	sslcrt string
+	sslkey string
+)
+
 func main() {
-	server.Run()
+	flag.StringVar(&mode, "mode", "", "run mode,server or client")
+	flag.StringVar(&port, "port", "9999", "server port")
+	flag.StringVar(&ip, "ip", "", "Listen ip")
+	flag.StringVar(&mode, "pass", "", "Connect password")
+	flag.StringVar(&mode, "sslcrt", "", "sslcrt")
+	flag.StringVar(&mode, "sslkey", "", "sslkey")
+	flag.Parse()
+	if mode == "server" {
+		server.Run()
+	} else if mode == "client" {
+		client.Run()
+	}
+
 }
